d7dc50d5307e1d0366ab4a0c9b6fe83cf0893db1
conrad93 <nunes.conrad93@gmail.com>
2024-10-23 23:18:49 +0530

    mat init

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MaterialModule } from './material/material.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MaterialModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ angular-client/src/app/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatSlideToggleModule
+  ],
+  exports: [
+    MatSlideToggleModule
+  ]
+})
+export class MaterialModule { }
